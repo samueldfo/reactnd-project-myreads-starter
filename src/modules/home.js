2eebe4d5fcb4a7245345dbd831f60a1e0c3386f7
Samuel Oliveira <samueldfo@gmail.com>
2018-01-21 15:19:49 -0200

    added index import and exports

---
+++ src/modules/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const test = (<div>teste</div>);
+
+export class Home extends Component {
+
+
+  render() {
+
+    return (
+      <div>
+        <h2>hello world</h2>
+      </div>
+    )
+  }
+
+}
