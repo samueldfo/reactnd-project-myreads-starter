cb95394188a654c0f8fe9ac078a9e99e8a3dffaf
Samuel Oliveira <samueldfo@gmail.com>
2018-01-21 16:02:38 -0200

    home and search link

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { BooksAPI } from './datasources'
-import { Home } from './modules'
+import { Home, Search } from './modules'
 import './App.css'
 
 
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+  // state = {
+  //   /**
+  //    * TODO: Instead of using this state variable to keep track of which page
+  //    * we're on, use the URL in the browser's address bar. This will ensure that
+  //    * users can use the browser's back and forward buttons to navigate between
+  //    * pages, as well as provide a good URL they can bookmark and share.
+  //    */
+  //   showSearchPage: false
+  // }
 
   render() {
     return (
@@ -23,6 +23,14 @@ class BooksApp extends React.Component {
       <div>
         <Route exact path='/' render={() => (
           <Home />
+        )} />
+        
+        <Route path='/search' render={({ history }) => (
+          <Search
+            onSearchBook={(book) => {
+              this.searchBook(book)
+              history.push('/')
+            }} />
         )} />
       </div>
     )
