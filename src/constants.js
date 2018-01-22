97c25e60764748e863657ea2c702450089cb9de9
Samuel Oliveira <samueldfo@gmail.com>
2018-01-22 01:13:46 -0200

    consuming Books API

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const shelfState = Object.freeze({
+  'currentlyReading': 'currentlyReading',
+  'wantToRead': 'wantToRead',
+  'read': 'read',
+  'none': 'none',
+})
+
+export const shelfTitle = Object.freeze({
+  'currentlyReading': 'Currently Reading',
+  'wantToRead': 'Want To Read',
+  'read': 'Read',
+  'none': 'None',
+})
